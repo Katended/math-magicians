a1ded99bbdeeaeb387a07718a9c5b067a92e70d9
David Katende <katendedavidjoga@gmail.com>
2023-06-09 16:44:47 +0300

    Add quote

---
+++ src/components/Quote.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+const Quotes = () => {
+  const [quotes, setQuotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const urlKey = '1huYYk+zqczgWbjc9JnDbw==v8vhqPo3lPwQJx8M';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes?limit=1&category=hope',
+          {
+            method: 'GET',
+            headers: { 'X-Api-Key': urlKey },
+          });
+        const res = await response.json();
+        setQuotes(res);
+      } catch (error) {
+        setHasError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [setHasError, setIsLoading, setQuotes]);
+
+  if (isLoading) return <div>Loading ...</div>;
+  if (hasError) return <div>Something Went Wrong!</div>;
+
+  return (
+    <div className="quotes-container">
+      <h1>Quotes</h1>
+      <ul>
+        {
+          quotes.map((quote) => (
+            <li key={quote.author}>{quote.quote}</li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default Quotes;
